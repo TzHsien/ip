07df8f2dce767170015b4dd3711a177ad9db232c
TzHsien <limtzehsien@gmail.com>
2025-08-22 18:56:26 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+how# Duke User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Friday.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Friday {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Friday");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
